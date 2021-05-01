eb72d6a96e72e6b4b7023c877b3c059cd0af05bd
Dominik Berner <dominik.berner@gmail.com>
2021-05-01 22:25:35 +0200

    Using organisms in cells

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ use tui::widgets::{Block, Borders, Row, Table};
 use tui::style::{Color, Style};
 use tui::Terminal;
 
+const WORLD_WIDTH: usize = 50;
+const WORLD_HEIGHT: usize = 20;
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let world = vec![vec![0; 50]; 20];
+    let organisms = vec![vec!["X"; WORLD_WIDTH]; WORLD_HEIGHT];
+    //let world = vec!["X"; WORLD_WIDTH];
 
     let return_value = terminal.draw(|f| {
         let chunks = Layout::default()
@@ -22,17 +25,13 @@ fn main() -> Result<(), io::Error> {
             .split(f.size());
         let world = Table::new(vec![
             // Row can be created from simple strings.
-            Row::new(vec!["Row11", "Row12", "Row13"]),
+            Row::new(organisms[0].clone()),
             // You can style the entire row.
             Row::new(vec!["Row21", "Row22", "Row23"]).style(Style::default().fg(Color::Blue)),
         ])
         .style(Style::default().fg(Color::White))
         .block(Block::default().title("world").borders(Borders::ALL))
-        .widths(&[
-            Constraint::Length(1),
-            Constraint::Length(1),
-            Constraint::Length(1),
-        ])
+        .widths(&[Constraint::Length(1); WORLD_WIDTH])
         .column_spacing(0);
         f.render_widget(world, chunks[0]);
         let log = Block::default().title("log").borders(Borders::ALL);
