602d11f7289f3c2a93b45cb2482ea05e720c7edb
Dominik Berner <dominik.berner@bbv.ch>
2021-05-06 10:29:27 +0200

    Move draw to own function

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,29 @@ enum Event<I> {
     Tick,
 }
 
+fn draw<B : tui::backend::Backend>(terminal: &mut Terminal<B>, organisms : &Vec<Vec<&str>>) -> std::result::Result<(), io::Error>
+{
+    terminal.draw(|f| {
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(1)
+            .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
+            .split(f.size());
+        let rows = organisms.iter().map(|item| {
+            let cells = item.iter().map(|c| Cell::from(*c));
+            Row::new(cells).style(Style::default().fg(Color::Blue))
+        });
+        let world = Table::new(rows)
+            .style(Style::default().fg(Color::White))
+            .block(Block::default().title("world").borders(Borders::ALL))
+            .widths(&[Constraint::Length(1); WORLD_WIDTH])
+            .column_spacing(0);
+        f.render_widget(world, chunks[0]);
+        let log = Block::default().title("log").borders(Borders::ALL);
+        f.render_widget(log, chunks[1]);
+    })
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     enable_raw_mode().expect("can run in raw mode");
     let stdout = io::stdout();
@@ -67,25 +90,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
     loop {
-        terminal.draw(|f| {
-            let chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .margin(1)
-                .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
-                .split(f.size());
-            let rows = organisms.iter().map(|item| {
-                let cells = item.iter().map(|c| Cell::from(*c));
-                Row::new(cells).style(Style::default().fg(Color::Blue))
-            });
-            let world = Table::new(rows)
-                .style(Style::default().fg(Color::White))
-                .block(Block::default().title("world").borders(Borders::ALL))
-                .widths(&[Constraint::Length(1); WORLD_WIDTH])
-                .column_spacing(0);
-            f.render_widget(world, chunks[0]);
-            let log = Block::default().title("log").borders(Borders::ALL);
-            f.render_widget(log, chunks[1]);
-        })?;
+        draw(&mut terminal, &organisms)?;
 
         match rx.recv()? {
             Event::Input(event) => match event.code {
