4d1ab04891c67faa90452fd316357da4a7d429bd
Dominik Berner <dominik.berner@bbv.ch>
2021-06-10 06:46:17 +0200

    Add function to find closest empty cell

--- src/world.rs
+++ src/world.rs
@@ -17,6 +17,9 @@ fn find_closest_empty_index(world: &Vec<Organism>, start_idx: usize) -> Result<u
     if start_idx > world.len(){
         panic!("start_index out of bounds {}", world.len());
     }
+    if(!world[start_idx].is_alive()){
+        return Ok(start_idx)
+    }
     if start_idx == 1{
         Err("No space left in world".to_owned())
     }
@@ -40,6 +43,19 @@ mod tests {
 
     #[test]
     fn test_find_closest_returns_err_if_no_empty_fields()
+    {
+        let mut world : Vec<Organism> = Vec::with_capacity(3);
+        for i in 0..world.capacity()
+        {
+            world.push(Organism::new("a".to_owned()));
+        }
+        
+        assert!(find_closest_empty_index(&world, 1).is_err());
+    }
+
+
+    #[test]
+    fn test_find_closest_returns_same_index_if_cell_is_empty()
     {
         let mut world : Vec<Organism> = Vec::with_capacity(3);
         for i in 0..world.capacity()
