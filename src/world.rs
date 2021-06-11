c8a7c323a71f33d5544d4f779d8c685d2f0482dd
Dominik Berner <dominik.berner@bbv.ch>
2021-06-11 10:24:52 +0200

    Forward and backwards search works

--- src/world.rs
+++ src/world.rs
@@ -12,22 +12,28 @@ pub fn insert_close_to_parent(organism: Organism, world: &mut Vec<Organism>, idx
     }
 }
 
-fn find_closest_empty_index(world: &Vec<Organism>, start_idx: usize) -> Result<usize, String>
-{
-    if start_idx > world.len(){
+fn find_closest_dead_index(world: &Vec<Organism>, start_idx: usize) -> Result<usize, String> {
+    if start_idx > world.len() {
         panic!("start_index out of bounds {}", world.len());
     }
-    if !world[start_idx].is_alive()
-    {
+    // return self if dead
+    if !world[start_idx].is_alive() {
         return Ok(start_idx);
     }
-    if start_idx == 1{
-        Err("No space left in world".to_owned())
+    // search next dead cell forward
+    for i in start_idx..world.len() {
+        if !world[i].is_alive() {
+            return Ok(i);
+        }
     }
-    else{
-        Ok(0)
+    // search next dead cell backwards
+    for i in (0..start_idx).rev() {
+        if !world[i].is_alive() {
+            return Ok(i);
+        }
     }
 
+    Err("No space left in world".to_owned())
 }
 
 #[cfg(test)]
@@ -36,62 +42,47 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_find_closest_fails_if_index_out_of_bounds()
-    {
-        let world : Vec<Organism> = Vec::new(); 
-        assert!(find_closest_empty_index(&world, 1).is_err());
+    fn test_find_closest_fails_if_index_out_of_bounds() {
+        let world: Vec<Organism> = Vec::new();
+        assert!(find_closest_dead_index(&world, 1).is_err());
     }
 
     #[test]
-    fn test_find_closest_returns_err_if_no_empty_fields()
-    {
-        let mut world : Vec<Organism> = Vec::with_capacity(3);
-        for _i in 0..world.capacity()
-        {
+    fn test_find_closest_returns_err_if_no_dead_fields() {
+        let mut world: Vec<Organism> = Vec::with_capacity(3);
+        for _i in 0..world.capacity() {
             world.push(Organism::new("a".to_owned()));
         }
-        
-        assert!(find_closest_empty_index(&world, 1).is_err());
+        assert!(find_closest_dead_index(&world, 1).is_err());
     }
 
-
     #[test]
-    fn test_find_closest_returns_same_index_if_cell_is_empty()
-    {
-        let mut world : Vec<Organism> = Vec::with_capacity(3);
-        for _i in 0..world.capacity()
-        {
+    fn test_find_closest_returns_same_index_if_cell_is_dead() {
+        let mut world: Vec<Organism> = Vec::with_capacity(3);
+        for _i in 0..world.capacity() {
             world.push(Organism::new("a".to_owned()));
         }
         world[1].genome = "".to_owned();
-        
-        assert_eq!(find_closest_empty_index(&world, 1).unwrap(), 1);
+        assert_eq!(find_closest_dead_index(&world, 1).unwrap(), 1);
     }
 
     #[test]
-    fn test_find_closest_returns_next_index_upwards_if_cell_is_empty()
-    {
-        let mut world : Vec<Organism> = Vec::with_capacity(3);
-        for _i in 0..world.capacity()
-        {
+    fn test_find_next_dead_cell_if_only_one_is_free() {
+        let mut world: Vec<Organism> = Vec::with_capacity(3);
+        for _i in 0..world.capacity() {
             world.push(Organism::new("a".to_owned()));
         }
-        world[0].genome = "".to_owned();
-        world[2].genome = "".to_owned();
-        
-        assert_eq!(find_closest_empty_index(&world, 1).unwrap(), 2);
+        let last_idx = world.len() - 1;
+        world[last_idx].genome = "".to_owned();
+        assert_eq!(find_closest_dead_index(&world, 1).unwrap(), last_idx);
     }
     #[test]
-    fn test_find_closest_returns_next_index_downwards_if_upwards_cell_is_not_empty()
-    {
-        let mut world : Vec<Organism> = Vec::with_capacity(3);
-        for _i in 0..world.capacity()
-        {
+    fn test_find_closest_returns_dead_cell_if_only_one_is_free_and_dead_cell_is_lower_in_index() {
+        let mut world: Vec<Organism> = Vec::with_capacity(3);
+        for _i in 0..world.capacity() {
             world.push(Organism::new("a".to_owned()));
         }
         world[0].genome = "".to_owned();
-        
-        
-        assert_eq!(find_closest_empty_index(&world, 1).unwrap(), 2);
+        assert_eq!(find_closest_dead_index(&world, 1).unwrap(), 0);
     }
 }
